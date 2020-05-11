097333e862a934839506eed09a245f3bdfa57229
Martin-Shao <shaogucheng@hotmail.com>
2020-05-12 01:09:41 +0800

    update: 实验useReducer一些功能和特性

---
+++ src/components/Toggle/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import Switch from '@material-ui/core/Switch';
+import Button from '@material-ui/core/Button'
+
+const noop = () => { }
+
+// action
+const actionTypes = {
+    toggle: 'TOGGLE',
+    on: 'ON',
+    off: 'OFF',
+}
+
+function toggleReducer(state, action) {
+    console.info('toggleReducer...')
+    switch (action.type) {
+        case actionTypes.toggle: {
+            return { on: !state.on }
+        }
+        case actionTypes.on: {
+            return { on: true }
+        }
+        case actionTypes.off: {
+            return { on: false }
+        }
+        default:
+            throw new Error(`Unhandled type: ${action.type}`)
+    }
+}
+
+function useToggle({ reducer = toggleReducer } = {}) {
+    const [{ on }, dispatch] = React.useReducer(reducer, { on: false })
+
+    const toggle = () => dispatch({ type: actionTypes.toggle })
+    const setOn = () => dispatch({ type: actionTypes.on })
+    const setOff = () => dispatch({ type: actionTypes.off })
+
+    return { on, toggle, setOn, setOff }
+}
+
+function Toggle() {
+    const [clicksSinceReset, setClicksSinceReset] = React.useState(0)
+    const tooManyClicks = clicksSinceReset >= 4
+    console.info('clicksSinceReset...', clicksSinceReset)
+    const { on, toggle, setOn, setOff } = useToggle({
+        reducer(currentState, action) {
+            const changes = toggleReducer(currentState, action)
+            console.info('action type...', action.type)
+            if (tooManyClicks && action.type === actionTypes.toggle) {
+                // other changes are fine, but on needs to be unchanged
+                console.info('too many clicks')
+                return { ...changes, on: currentState.on }
+            } else {
+                // the changes are fine
+                return changes
+            }
+        }
+    })
+
+    return (
+        <div style={{ padding: 20, margin: 20, backgroundColor: '#f9f9f9' }}>
+            <Button style={{ marginRight: 12 }} variant="contained" color="primary" onClick={setOff}>Switch Off</Button>
+            <Button variant="contained" color="secondary" onClick={setOn}>Switch on</Button>
+            <Switch
+                color="primary"
+                onChange={noop}
+                onClick={() => {
+                    toggle()
+                    setClicksSinceReset(count => count + 1)
+                }}
+                checked={on}
+            />
+            {
+                tooManyClicks ? (
+                    <Button variant="contained" color="primary" onClick={() => setClicksSinceReset(0)}>Reset</Button>
+                ) : null
+            }
+        </div>
+    )
+}
+
+export default Toggle
--- src/router/App/index.jsx
+++ src/router/App/index.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import React from 'react'
-import TaskPage from '../../pages/TaskPage'
+// import TaskPage from '../../pages/TaskPage'
+import Toggle from '../../components/Toggle'
+// import TestSelector from '../../pages/TestSelector'
 
 function App() {
   return (
-    <TaskPage />
+    <Toggle />
   )
 }
 

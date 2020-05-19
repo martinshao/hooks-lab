7c340e0538761a5fa207fc122c6a7ec7e522e246
Martin <shaogucheng@hotmail.com>
2020-05-19 09:34:43 +0800

    hooks 实验

--- src/router/App/index.jsx
+++ src/router/App/index.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import React from 'react'
-import TaskPage from '../../pages/TaskPage'
+// import TaskPage from '../../pages/TaskPage'
+import UserInfo from '../../pages/UserInfo'
 
 function App() {
   return (
-    <TaskPage />
+    <UserInfo />
   )
 }
 
